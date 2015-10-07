49deb2c5c544743b9b26abe2093cf0176dbecb1a
scotchfield <swrittenb@gmail.com>
2015-10-07 09:56:44 -0400

    Add more JS unit tests and shotgun tests
    The addComments functions are not resilient, so they're about to
    get some well-deserved attention.

--- tests/qunit/tests.js
+++ tests/qunit/tests.js
@@ -5,6 +5,13 @@ QUnit.test('Basic rocketComment object tests', function(assert) {
 	assert.ok(rocketComments.CommentsCollection);
 	assert.ok(rocketComments.CommentsView);
 });
+QUnit.test('Basic addComment object tests', function(assert) {
+	assert.ok(addComment);
+	assert.ok(addComment.setupForm);
+	assert.ok(addComment.resetForm);
+	assert.ok(addComment.moveForm);
+	assert.ok(addComment.editForm);
+});
 QUnit.test('Basic CommentModel testing', function(assert) {
 	var model = new rocketComments.CommentModel();
 	assert.ok(model);
@@ -18,6 +25,9 @@ QUnit.test('Basic CommentModel testing', function(assert) {
 	model.initialize();
 	assert.ok(model.get('edit'));
 	assert.notOk(model.get('edit_class'));
+
+	assert.ok('url' in model);
+	assert.ok(model.url().indexOf('/comments/') > -1);
 });
 QUnit.test('Basic CommentView testing', function(assert) {
 	assert.ok(new rocketComments.CommentView());
@@ -29,6 +39,14 @@ QUnit.test('Basic CommentsView testing', function(assert) {
 	assert.ok(new rocketComments.CommentsView());
 });
 QUnit.test('Shotgun testing', function(assert) {
+	assert.ok(shotgun(rocketComments.start))
+	assert.ok(shotgun(rocketComments.shiftPage))
+
+	assert.ok(shotgun(addComment.setupForm));
+	assert.ok(shotgun(addComment.resetForm));
+	assert.ok(shotgun(addComment.moveForm));
+	assert.ok(shotgun(addComment.editForm));
+
 	var model = new rocketComments.CommentModel();
 	assert.ok(shotgun(_.bind(model.initialize, model)))
 	assert.ok(shotgun(_.bind(model.sync, model)))
