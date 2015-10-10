0c4e712cca0480fb2f54d09787c1f9b85552e6ae
scotchfield <swrittenb@gmail.com>
2015-10-09 22:07:34 -0400

    Improve formatting for rocketComments

--- js/rocket-comments.js
+++ js/rocket-comments.js
@@ -3,25 +3,25 @@
 var rocketComments = rocketComments || {};
 
 rocketComments.start = function () {
-	jQuery('#wp-loading').show();
+	jQuery( '#wp-loading' ).show();
 
-	if (undefined === rocketComments.commentsView) {
+	if ( undefined === rocketComments.commentsView ) {
 		rocketComments.commentsView = new rocketComments.views.Comments();
 	}
 
-	jQuery('.comment-navigation .nav-previous a').on('click', function () {
-		rocketComments.shiftPage(-1);
-	});
-	jQuery('.comment-navigation .nav-next a').on('click', function () {
-		rocketComments.shiftPage(1);
-	});
+	jQuery( '.comment-navigation .nav-previous a' ).on( 'click', function () {
+		rocketComments.shiftPage( -1 );
+	} );
+	jQuery( '.comment-navigation .nav-next a' ).on( 'click', function () {
+		rocketComments.shiftPage( 1 );
+	} );
 
-	jQuery('form#commentform').submit(function (e) {
+	jQuery( 'form#commentform' ).submit(function ( e ) {
 		e.preventDefault();
 	});
 };
 
-rocketComments.shiftPage = function (delta) {
+rocketComments.shiftPage = function ( delta ) {
 	rocketComments.commentsView.comment_page += delta;
-	rocketComments.commentsView.fetchComments(rocketComments.commentsView.collection);
+	rocketComments.commentsView.fetchComments( rocketComments.commentsView.collection );
 };
