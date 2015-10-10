eb22b8a3227f069a879cc158590d0bd80a1a5136
scotchfield <swrittenb@gmail.com>
2015-10-09 22:03:48 -0400

    Improve formatting for collections

--- js/collections/Comments.js
+++ js/collections/Comments.js
@@ -6,26 +6,26 @@ rocketComments.collections = rocketComments.collections || {};
 rocketComments.collections.Comments = (function () {
 	var collection = wp.api.collections.Comments.extend({
 		initialize: function () {
-			this.order = jQuery('.comments-area').data('comment-order');
+			this.order = jQuery( '.comments-area' ).data( 'comment-order' );
 			this.model = rocketComments.models.Comment;
 			this.state = {};
 		},
 
 		url: function () {
-			if (this.post_id) {
+			if ( this.post_id ) {
 				return WP_API_Settings.root + '/comments/?orderby=id&order=' + this.order + '&post=' + this.post_id;
 			}
 			return WP_API_Settings.root + '/comments/';
 		},
 
-		commentDepth: function (item) {
+		commentDepth: function ( item ) {
 			var depth = 1;
-			while (item && item.get('parent') > 0) {
-				item = this.where({id: item.get('parent')})[0];
+			while ( item && item.get( 'parent' ) > 0 ) {
+				item = this.where({ id: item.get( 'parent' ) })[0];
 				depth += 1;
 			}
-			if (! this.threaded) {
-				this.threaded = jQuery('div#comments').data('threaded');
+			if ( ! this.threaded ) {
+				this.threaded = jQuery( 'div#comments' ).data( 'threaded' );
 			}
 			return depth < this.threaded ? depth : this.threaded;
 		}
