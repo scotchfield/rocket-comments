eace97c4222f02ee724d333dc612ada548d65a69
scotchfield <swrittenb@gmail.com>
2015-10-18 15:28:19 -0400

    Add some JSDoc markup to the main file

--- js/rocket-comments.js
+++ js/rocket-comments.js
@@ -2,6 +2,9 @@
 
 var rocketComments = rocketComments || {};
 
+/**
+ * Set up the initial Rocket Comments environment and handlers.
+ */
 rocketComments.start = function () {
 	jQuery( '#wp-loading' ).show();
 
@@ -9,6 +12,7 @@ rocketComments.start = function () {
 		rocketComments.commentsView = new rocketComments.views.Comments();
 	}
 
+	// Attach click handlers to the previous and next page elements.
 	jQuery( '.comment-navigation .nav-previous a' ).on( 'click', function () {
 		rocketComments.shiftPage( -1 );
 	} );
@@ -16,11 +20,16 @@ rocketComments.start = function () {
 		rocketComments.shiftPage( 1 );
 	} );
 
+	// Don't use the standard form submit; we'll trigger this using JS.
 	jQuery( 'form#commentform' ).submit(function ( e ) {
 		e.preventDefault();
 	});
 };
 
+/**
+ * Increase or decrease the current page via navigation bar click events.
+ * @param {number} delta - Number of pages to shift, may be negative.
+ */
 rocketComments.shiftPage = function ( delta ) {
 	this.commentsView.comment_page += delta;
 	this.commentsView.fetchComments( this.commentsView.collection );
