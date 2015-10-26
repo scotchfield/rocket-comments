2eea5649a4a7ab2804eca748f9d649da20483d74
scotchfield <swrittenb@gmail.com>
2015-10-26 16:28:22 -0400

    Add additional escaping in comment templates

--- templates/comment-below.php
+++ templates/comment-below.php
@@ -5,8 +5,8 @@
 				<img alt src="<%= attributes.author_avatar_urls['56'] %>" class="avatar" height="56" width="56" />
 				<?php echo '<b class="fn"><a href="<%= attributes.author_url %>" rel="external nofollow" class="url"><%= attributes.author_name %></a></b>'; ?>
 			</div>
-			<% if (attributes.status == 'hold') { %>
-				<p class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', 'rocket-comments' ); ?></p>
+			<% if ( attributes.status == 'hold' ) { %>
+				<p class="comment-awaiting-moderation"><?php esc_html_e( 'Your comment is awaiting moderation.', 'rocket-comments' ); ?></p>
 			<% } %>
 		</footer>
 		<div class="comment-content">
@@ -30,19 +30,19 @@
 <?php
 if ( get_option( 'comment_registration' ) && ! is_user_logged_in() ) {
 ?>
-					<a rel="nofollow" class="comment-reply-login" href="<?php echo esc_url( wp_login_url( get_permalink() ) ); ?>"><?php _e( 'Log in to Reply', 'rocket-comments' ); ?></a>
+					<a rel="nofollow" class="comment-reply-login" href="<?php echo esc_url( wp_login_url( get_permalink() ) ); ?>"><?php esc_html_e( 'Log in to Reply', 'rocket-comments' ); ?></a>
 <?php
 } else {
 ?>
-					<a rel='nofollow' class='comment-reply-link' href='<?php echo get_permalink(); ?>?replytocom=<%= attributes.id %>#respond' onclick='return addComment.moveForm( "div-comment-<%= attributes.id %>", "<%= attributes.id %>", "respond", "<%= attributes.id %>" )' aria-label='<?php _e( 'Leave a Reply', 'rocket-comments' ); ?>'><?php _e( 'Reply', 'rocket-comments' ); ?></a>
+					<a rel='nofollow' class='comment-reply-link' href='<?php echo esc_url( get_permalink() ); ?>?replytocom=<%= attributes.id %>#respond' onclick='return addComment.moveForm( "div-comment-<%= attributes.id %>", "<%= attributes.id %>", "respond", "<%= attributes.id %>" )' aria-label='<?php esc_html_e( 'Leave a Reply', 'rocket-comments' ); ?>'><?php esc_html_e( 'Reply', 'rocket-comments' ); ?></a>
 <?php
 }
 ?>
 				</li>
 				<li class="edit-link">
 					<div class="genericon genericon-edit"></div>
-					<a class="comment-edit-link" href="<?php echo get_admin_url() ?>comment.php?action=editcomment&amp;c=<%= attributes.id %>" onclick='return addComment.editForm(<%= attributes.id %>, "respond");' data-id="<%= attributes.id %>">
-						<?php _e( 'Edit', 'rocket-comments' ); ?>
+					<a class="comment-edit-link" href="<?php echo esc_url( get_admin_url() ); ?>comment.php?action=editcomment&amp;c=<%= attributes.id %>" onclick='return addComment.editForm(<%= attributes.id %>, "respond");' data-id="<%= attributes.id %>">
+						<?php esc_html_e( 'Edit', 'rocket-comments' ); ?>
 					</a>
 				</li>
 			</ul>
--- templates/comment-default.php
+++ templates/comment-default.php
@@ -3,7 +3,7 @@
 		<footer class="comment-meta">
 			<div class="comment-author vcard">
 				<img alt src="<%= attributes.author_avatar_urls['56'] %>" class="avatar" height="56" width="56" />
-				<?php _e( sprintf( '%s <span class="says">says:</span>', '<b class="fn"><a href="<%= attributes.author_url %>" rel="external nofollow" class="url"><%= attributes.author_name %></a></b>' ), 'rocket-comments' ); ?>
+				<?php printf( __( '%s <span class="says">says:</span>', 'rocket-comments' ), '<b class="fn"><a href="<%= attributes.author_url %>" rel="external nofollow" class="url"><%= attributes.author_name %></a></b>' ); ?>
 			</div>
 			<div class="comment-metadata">
 				<a href="<%= attributes.link %>">
@@ -15,13 +15,13 @@
 					</time>
 				</a>
 				<span class="edit-link <%= attributes.edit === 0 ? 'hidden' : '' %>">
-					<a class="comment-edit-link" href="<?php echo get_admin_url() ?>comment.php?action=editcomment&amp;c=<%= attributes.id %>" onclick='return addComment.editForm(<%= attributes.id %>, "respond");' data-id="<%= attributes.id %>">
-						<?php _e( 'Edit', 'rocket-comments' ); ?>
+					<a class="comment-edit-link" href="<?php echo esc_url( get_admin_url() ); ?>comment.php?action=editcomment&amp;c=<%= attributes.id %>" onclick='return addComment.editForm(<%= attributes.id %>, "respond");' data-id="<%= attributes.id %>">
+						<?php esc_html_e( 'Edit', 'rocket-comments' ); ?>
 					</a>
 				</span>
 			</div>
-			<% if (attributes.status == 'hold') { %>
-				<p class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.', 'rocket-comments' ); ?></p>
+			<% if ( attributes.status == 'hold' ) { %>
+				<p class="comment-awaiting-moderation"><?php esc_html_e( 'Your comment is awaiting moderation.', 'rocket-comments' ); ?></p>
 			<% } %>
 		</footer>
 		<div class="comment-content">
@@ -31,11 +31,11 @@
 <?php
 if ( get_option( 'comment_registration' ) && ! is_user_logged_in() ) {
 ?>
-			<a rel="nofollow" class="comment-reply-login" href="<?php echo esc_url( wp_login_url( get_permalink() ) ); ?>"><?php _e( 'Log in to Reply', 'rocket-comments' ); ?></a>
+			<a rel="nofollow" class="comment-reply-login" href="<?php echo esc_url( wp_login_url( get_permalink() ) ); ?>"><?php esc_html_e( 'Log in to Reply', 'rocket-comments' ); ?></a>
 <?php
 } else {
 ?>
-			<a rel='nofollow' class='comment-reply-link' href='<?php echo get_permalink(); ?>?replytocom=<%= attributes.id %>#respond' onclick='return addComment.moveForm( "div-comment-<%= attributes.id %>", "<%= attributes.id %>", "respond", "<%= attributes.id %>" )' aria-label='<?php _e( 'Leave a Reply', 'rocket-comments' ); ?>'><?php _e( 'Reply', 'rocket-comments' ); ?></a>
+			<a rel='nofollow' class='comment-reply-link' href='<?php echo esc_url( get_permalink() ); ?>?replytocom=<%= attributes.id %>#respond' onclick='return addComment.moveForm( "div-comment-<%= attributes.id %>", "<%= attributes.id %>", "respond", "<%= attributes.id %>" )' aria-label='<?php esc_html_e( 'Leave a Reply', 'rocket-comments' ); ?>'><?php esc_html_e( 'Reply', 'rocket-comments' ); ?></a>
 <?php
 }
 ?>
